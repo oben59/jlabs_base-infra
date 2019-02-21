bc6dcfbb880ac2bcabc255aa65ac9175d17c010a
jdxlabs <jdevoucoux@gmail.com>
2019-02-21 15:53:26 +0100

    set mfa-connect

---
+++ mfa-connect.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+unset AWS_DEFAULT_REGION
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
+if [[ $# -ne 1 ]]; then
+    echo "usage: $0 <current_env> (e.g. \`$0 jdxlabs\`)" > /dev/stderr
+    exit 127
+fi
+
+MASTER_KEY_NAME=aws-jdxmaster
+TOTP_KEY=$TOTP_KEY_JDX_USER
+
+if [ "$1" == "prod" ]; then
+    DISTANTACCOUNT="586212427949"
+    ROLENAME="Administrator"
+elif [ "$1" == "dev" ]; then
+    DISTANTACCOUNT=""
+    ROLENAME=""
+else
+    echo "$0 error: Please current env should be one of them : 'dev', 'prod'." > /dev/stderr
+    exit 127
+fi
+
+if ! which jq 2>&1 > /dev/null ; then
+    echo "$0 error: Please install 'jq'." > /dev/stderr
+    exit 127
+fi
+
+if ! which aws 2>&1 > /dev/null ; then
+    echo "$0 error: Please install 'awscli'." > /dev/stderr
+    exit 127
+fi
+
+# Checks if the correct profile is selected
+export AWS_PROFILE=$MASTER_KEY_NAME
+AWSNAME="$(echo $AWS_PROFILE)"
+if [ $AWS_PROFILE != $MASTER_KEY_NAME ]; then
+    echo "$0 error: incorrect profile found, please switch on this one :" > /dev/stderr
+    echo "export AWS_PROFILE=$MASTER_KEY_NAME" > /dev/stderr
+    exit 127
+fi
+
+# Get user arn
+USERARN="$(aws --output json iam get-user | jq -r .User.Arn)"
+if [[ -z "$USERARN" ]]; then
+    echo "$0 error: unable to determine AWS IAM user.  Did you run 'aws configure'?" > \
+        /dev/stderr
+    exit 128
+fi
+
+CURRENT_ENV=$1
+REGION="eu-west-1"
+ROLEARN="arn:aws:iam::$DISTANTACCOUNT:role/$ROLENAME"
+IAMUSER="$(basename $USERARN)"
+ACCOUNT="$(echo $USERARN | cut -d: -f5)"
+MFAARN="arn:aws:iam::$ACCOUNT:mfa/$IAMUSER"
+
+# echo -n "Enter MFA token code for $MFAARN: "
+# read MFACODE
+# echo ""
+MFACODE="$(oathtool --totp --base32 $TOTP_KEY)"
+
+# assume role for distant account
+RESP="$(aws --region $REGION sts assume-role \
+    --role-arn $ROLEARN \
+    --role-session-name assumption-$IAMUSER-$(date +%s) \
+    --serial-number $MFAARN \
+    --token-code $MFACODE 2> /dev/null )"
+
+AKI="$(echo $RESP | jq -r .Credentials.AccessKeyId)"
+if [[ -z "$AKI" ]]; then
+    echo "Failure." > /dev/stderr
+    exit 129
+fi
+SAK="$(echo $RESP | jq -r .Credentials.SecretAccessKey)"
+ST="$(echo $RESP | jq -r .Credentials.SessionToken)"
+
+echo "export AWS_PROFILE=$MASTER_KEY_NAME"
+echo "export AWS_DEFAULT_REGION=\"$REGION\""
+export AWS_DEFAULT_REGION="$REGION"
+echo "export AWS_ACCESS_KEY_ID=\"$AKI\""
+export AWS_ACCESS_KEY_ID="$AKI"
+echo "export AWS_SECRET_ACCESS_KEY=\"$SAK\""
+export AWS_SECRET_ACCESS_KEY="$SAK"
+echo "export AWS_SESSION_TOKEN=\"$ST\""
+export AWS_SESSION_TOKEN="$ST"
